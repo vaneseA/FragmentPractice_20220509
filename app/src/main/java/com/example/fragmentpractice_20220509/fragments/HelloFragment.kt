03d1e6aa652c28828394b77f3cae6ad59031a6ea
DK <spol@naver.com>
2022-05-09 11:27:16 +0900

    viewPager 재료용 프래그먼트 xml / 클래스 여러개 만들기

---
+++ app/src/main/java/com/example/fragmentpractice_20220509/fragments/HelloFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fragmentpractice_20220509.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice_20220509.R
+
+class HelloFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+}
