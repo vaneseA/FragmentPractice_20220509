878158f6235547a81a431bbb85daf53a4a867874
DK <spol@naver.com>
2022-05-09 12:07:51 +0900

    MainActivity에서 어댑터 달아주면서 화면 송출

--- app/src/main/java/com/example/fragmentpractice_20220509/MainActivity.kt
+++ app/src/main/java/com/example/fragmentpractice_20220509/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.fragmentpractice_20220509
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.fragmentpractice_20220509.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter: MainViewPagerAdapter
+    var mList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mAdapter
     }
 }
